2c9572593410f2f82b883c4ff43f144f28ab2268
omairvaiyani <um11mov@leeds.ac.uk>
2014-08-07 13:56:27 +0200

    Omair

--- app/router.js
+++ app/router.js
@@ -9,6 +9,7 @@ var Router = Ember.Router.extend({
 
 Router.map(function () {
     this.route('application');
+    //this.resource('index', {path : '/:'})
     this.route('about');
     this.route('case-studies');
     this.route('disclaimer');
