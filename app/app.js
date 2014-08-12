b5567ee6101b6d3d6f075b935028725670f57327
omairvaiyani <um11mov@leeds.ac.uk>
2014-08-12 16:44:36 +0100

    Fixed footer and firefox bug

--- app/app.js
+++ app/app.js
@@ -6,7 +6,12 @@ Ember.MODEL_FACTORY_INJECTIONS = true;
 
 var App = Ember.Application.extend({
   modulePrefix: 'cfgmedicine', // TODO: loaded via config
-  Resolver: Resolver
+  Resolver: Resolver,
+  /*
+   * This allows us to set CSS styling directly on the App wrapper
+   * Use case: pushing footer to the bottom of the page
+   */
+  rootElement: '#body-wrap'
 });
 
 loadInitializers(App, 'cfgmedicine');
